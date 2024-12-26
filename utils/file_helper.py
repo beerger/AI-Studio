c0d332536c66115d84d9a7510fbd49b79ec69a7c
William Berger <william.berger.7099@student.uu.se>
2024-12-26 03:15:01 +0100

    Helper functions for file handling

---
+++ utils/file_helper.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import json
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+def get_absolute_path(directory, filename):
+    """
+    Get the absolute path of a file relative to the project root.
+    """
+    return str(PROJECT_ROOT / directory / filename).replace("\\", "/")
+
+def load_json(directory, filename):
+    """
+    Load a JSON file from the specified directory.
+    """
+    with open(get_absolute_path(directory, filename), "r") as json_file:
+        return json.load(json_file)
+    
+def get_value_from_json(json_data, path):
+    for key in path:
+        json_data = json_data[key]
+    return json_data
